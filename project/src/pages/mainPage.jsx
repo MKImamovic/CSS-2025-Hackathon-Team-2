b94426375a7798f1376e066368a36fae40d3b74a
coderekorder <malik.ahmetbegovic@gmail.com>
2025-09-06 12:29:53 +0200

    First commit

---
+++ project/src/pages/mainPage.jsx
@@ -0,0 +1,13 @@
+import countries  from "../data/Countries"
+const mainPage = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    
+  return (
+   <div className="bg-[FAEDCE]">
+     mainPage
+        
+   </div>
+  )
+}
+
+export default mainPage
