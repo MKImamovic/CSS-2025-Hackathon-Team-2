d00e0a0f1b2cbb66f6cd3ddcd73943ec5ec78392
MKImamovic <makimamoviczenica@gmail.com>
2025-09-06 13:14:23 +0200

    https://openchargemap.org/site/develop/api

--- project/src/pages/mainPage.jsx
+++ project/src/pages/mainPage.jsx
@@ -1,19 +1,38 @@
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { useState, useEffect } from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100vw',
   height: '100vh'
 };
 
-const center = {
-  lat: 45.815399, 
-  lng: 15.966568  
+const defaultCenter = {
+  lat: 45.815399,
+  lng: 15.966568
 };
 
 export default function MainPage() {
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: 'AIzaSyCp77wvyFv-bOuCamN78fvonoCEXQCc5_8' 
+    googleMapsApiKey: 'AIzaSyCp77wvyFv-bOuCamN78fvonoCEXQCc5_8'
   });
+
+  const [center, setCenter] = useState(defaultCenter);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCenter({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          });
+        },
+        () => {
+          // If user denies geolocation, keep default center
+        }
+      );
+    }
+  }, []);
 
   if (!isLoaded) {
     return <div>Loading...</div>;
@@ -25,7 +44,7 @@ export default function MainPage() {
       center={center}
       zoom={12}
     >
-     
+      <Marker position={center} />
     </GoogleMap>
   );
-};
+}
