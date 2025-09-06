4ac844ca5039d4e1376271917ced67b8f15a90e8
MKImamovic <makimamoviczenica@gmail.com>
2025-09-06 11:47:38 +0200

    https://colorhunt.co/palette/ccd5aee0e5b6faedcefefae0

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,13 +1,10 @@
-
 function App() {
-  
-
   return (
     <>
       <div className="flex items-center justify-center h-screen w-screen bg-[#FAEDCE]">
-        <div classnmame="block">
-          <h1 className="text-5xl">VoltMap</h1>
-          <button></button>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-5xl mb-8">VoltMap</h1>
+          <button className="bg-[#E0E5B6] px-6 py-2 rounded w-100 h-20">Discover chargers around you</button>
         </div>
       </div>
     </>
