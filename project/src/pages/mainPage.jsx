f264b8813624246b507084577229af2d09215cb2
MKImamovic <makimamoviczenica@gmail.com>
2025-09-06 13:08:57 +0200

    maps

--- project/src/pages/mainPage.jsx
+++ project/src/pages/mainPage.jsx
@@ -1,6 +1,31 @@
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+
+const center = {
+  lat: 45.815399, 
+  lng: 15.966568  
+};
+
 export default function MainPage() {
-    return (
-      <>
-      </>
-    )
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AIzaSyCp77wvyFv-bOuCamN78fvonoCEXQCc5_8' 
+  });
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
   }
+
+  return (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={12}
+    >
+     
+    </GoogleMap>
+  );
+};
